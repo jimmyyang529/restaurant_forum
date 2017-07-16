0462d2606750558fef68d5c363d7ad42ff5af36d
jimmy <jimmyyang5296@gmail.com>
2017-07-16 14:32:18 +0800

    admin 做CRUD 餐廳

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -4,7 +4,41 @@ class Admin::RestaurantsController < ApplicationController
 
 
   def index
+    @restaurants = Restaurant.all
   end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    redirect_to :action => :index
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(restaurant_params)
+    redirect_to :action => :show, :id => @restaurant
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    redirect_to :action => :index
+  end
+
+
+
 
 
   protected
@@ -13,6 +47,10 @@ class Admin::RestaurantsController < ApplicationController
      authenticate_or_request_with_http_basic do |user_name, password|
          user_name == "username" && password == "password"
      end
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name,:tel,:address,:description,:image)
   end
 
 
